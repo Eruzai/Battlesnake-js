74b4bd2c966cc4b15fdedfdcb451632bba04284a
Kevin Levasseur <kevin_levasseur@telus.net>
2023-11-11 12:54:33 -0800

    updated color, head, tail, and author

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function info() {
 
   return {
     apiversion: "1",
-    author: "",       // TODO: Your Battlesnake Username
-    color: "#888888", // TODO: Choose color
-    head: "default",  // TODO: Choose head
-    tail: "default",  // TODO: Choose tail
+    author: "eruzai",       // Your Battlesnake Username
+    color: "#007BAD", // Choose color
+    head: "sand-worm",  // Choose head
+    tail: "sharp",  // Choose tail
   };
 }
 
